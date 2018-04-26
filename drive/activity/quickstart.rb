977a1bbbdc8d67102549024eb29dcd9575e50ea9
Grant Timmerman <granttimmerman@gmail.com>
2018-04-26 10:25:42 -0700

    Add Activity Quickstart

---
+++ drive/activity/quickstart.rb
@@ -0,0 +1,73 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# [START drive_activity_quickstart]
+require 'google/apis/appsactivity_v1'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
+require 'fileutils'
+
+OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
+APPLICATION_NAME = 'G Suite Activity API Ruby Quickstart'
+CLIENT_SECRETS_PATH = 'client_secret.json'
+CREDENTIALS_PATH = File.join(Dir.home, '.credentials',
+                             "appsactivity-ruby-quickstart.yaml")
+SCOPE = [Google::Apis::AppsactivityV1::AUTH_ACTIVITY,
+  Google::Apis::AppsactivityV1::AUTH_DRIVE_METADATA_READONLY]
+
+##
+# Ensure valid credentials, either by restoring from the saved credentials
+# files or intitiating an OAuth2 authorization. If authorization is required,
+# the user's default browser will be launched to approve the request.
+#
+# @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
+def authorize
+  FileUtils.mkdir_p(File.dirname(CREDENTIALS_PATH))
+
+  client_id = Google::Auth::ClientId.from_file(CLIENT_SECRETS_PATH)
+  token_store = Google::Auth::Stores::FileTokenStore.new(file: CREDENTIALS_PATH)
+  authorizer = Google::Auth::UserAuthorizer.new(
+    client_id, SCOPE, token_store)
+  user_id = 'default'
+  credentials = authorizer.get_credentials(user_id)
+  if credentials.nil?
+    url = authorizer.get_authorization_url(
+      base_url: OOB_URI)
+    puts "Open the following URL in the browser and enter the " +
+         "resulting code after authorization"
+    puts url
+    code = gets
+    credentials = authorizer.get_and_store_credentials_from_code(
+      user_id: user_id, code: code, base_url: OOB_URI)
+  end
+  credentials
+end
+
+# Initialize the API
+service = Google::Apis::AppsactivityV1::AppsactivityService.new
+service.client_options.application_name = APPLICATION_NAME
+service.authorization = authorize
+response = service.list_activities(source: 'drive.google.com',
+                                   drive_ancestor_id: 'root',
+                                   page_size: 10)
+puts "Recent activity:"
+puts "No resent activity" if response.activities.empty?
+response.activities.each do |activity|
+  event = activity.combined_event
+  user = event.user
+  target = event.target
+  next if user.nil? || target.nil?
+  time = Time.at(event.event_time_millis.to_i / 1000)
+  puts "#{time}: #{user.name} #{event.primary_event_type} #{target.name} #{target.mime_type}"
+end
+# [END drive_activity_quickstart]

64e39b1d77a7d9472fd836c61d704b61a591ba07
Eric Koleda <ekoleda@google.com>
2018-09-26 14:39:56 -0400

    Fix Tasks scope
    Fixes #13.

--- tasks/quickstart/quickstart.rb
+++ tasks/quickstart/quickstart.rb
@@ -21,7 +21,7 @@ OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'.freeze
 APPLICATION_NAME = 'Google Tasks API Ruby Quickstart'.freeze
 CREDENTIALS_PATH = 'credentials.json'.freeze
 TOKEN_PATH = 'token.yaml'.freeze
-SCOPE = Google::Apis::DriveV3::AUTH_TASKS_READONLY
+SCOPE = Google::Apis::TasksV1::AUTH_TASKS_READONLY
 
 ##
 # Ensure valid credentials, either by restoring from the saved credentials
